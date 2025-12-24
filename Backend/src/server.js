7314a8a9aa435aafb239ca8828d2ce0094e2935f
Arif2312323 <arifalmas3.14@gmail.com>
2025-12-24 20:27:26 +0530

    Finished with devlopment code

--- Backend/src/server.js
+++ Backend/src/server.js
@@ -1,22 +1,36 @@
 import express from "express";
-import { ENV } from "./lib/env.js";
 import path from "path";
+import cors from "cors";
+import { serve } from "inngest/express";
+import { clerkMiddleware } from "@clerk/express";
 
+import { ENV } from "./lib/env.js";
+import { connectDB } from "./lib/db.js";
+import { inngest, functions } from "./lib/inngest.js";
+
+import chatRoutes from "./routes/chatRoutes.js";
+import sessionRoutes from "./routes/sessionRoute.js";
 
 const app = express();
+
 const __dirname = path.resolve();
-const port = ENV.PORT;
-const NODE_ENV = ENV.NODE_ENV;
 
+// middleware
+app.use(express.json());
+// credentials:true meaning?? => server allows a browser to include cookies on request
+app.use(cors({ origin: ENV.CLIENT_URL, credentials: true }));
+app.use(clerkMiddleware()); // this adds auth field to request object: req.auth()
 
-console.log(port)
+app.use("/api/inngest", serve({ client: inngest, functions }));
+app.use("/api/chat", chatRoutes);
+app.use("/api/sessions", sessionRoutes);
 
-app.get("/health",(req,res)=>{
-    return res.status(200).json({msg:"Api working"});
-})
+app.get("/health", (req, res) => {
+  res.status(200).json({ msg: "api is up and running" });
+});
 
-
-if(NODE_ENV === "production") {
+// make our app ready for deployment
+if (ENV.NODE_ENV === "production") {
   app.use(express.static(path.join(__dirname, "../frontend/dist")));
 
   app.get("/{*any}", (req, res) => {
@@ -24,6 +38,13 @@ if(NODE_ENV === "production") {
   });
 }
 
-app.listen(port,()=>{ 
-    console.log("Server running on port");
-})
+const startServer = async () => {
+  try {
+    await connectDB();
+    app.listen(ENV.PORT, () => console.log("Server is running on port:", ENV.PORT));
+  } catch (error) {
+    console.error("💥 Error starting the server", error);
+  }
+};
+
+startServer();
